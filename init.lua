4e6d941df3233435475156cd619594f09460fc82
MorphZG <zoran1607@gmail.com>
2025-03-06 21:19:08 +0100

    add neo-tree and few dependancy plugins

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -232,7 +232,10 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'nvim-neo-tree/neo-tree.nvim',
+  'nvim-lua/plenary.nvim',
+  'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  'MunifTanjim/nui.nvim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
